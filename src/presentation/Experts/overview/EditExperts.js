551c34bf57362092bf917c7b61cc8ff85019b2be
Bhavyashree <bhavyashreep@techbyheart.in>
2021-09-14 10:34:37 +0530

    image uploading to firebase

--- src/presentation/Experts/overview/EditExperts.js
+++ src/presentation/Experts/overview/EditExperts.js
@@ -45,39 +45,43 @@ const EditCategory = () => {
         <BasicFormWrapper>
           <Form
             form={form}
-            id="createProject"
-            name="createProject"
-            onFinish={(values) => onfinish(values)}
+            id="editProject"
+            name="editProject"
+            onFinish={(values) =>console.log(values,"values")|| onfinish(values) }
           >
-            <Form.Item name="name">
-              <Input placeholder="Name" defaultValue={singleRow?.name} />
+            <Form.Item name="name"> 
+              {singleRow?.name && <Input name="name" placeholder="Name" defaultValue={singleRow?.name}  /> }
+              
             </Form.Item>
             <Form.Item name="email">
-              <Input placeholder="Email" defaultValue={singleRow?.email} />
+              <Input name="email" placeholder="Email" defaultValue={singleRow?.email} />
             </Form.Item>
             <Row gutter={15}>
               <Col md={12}>
                 <Form.Item name="title">
                   <Input
+                    name="title"
                     placeholder="Job Title"
-                    defaultValue={singleRow?.jobTitle}
+                    defaultValue={singleRow?.title}
                   />
                 </Form.Item>
               </Col>
               <Col md={12}>
                 <Form.Item name="phone">
-                  <Input placeholder="Phone" defaultValue={singleRow?.phone} />
+                  <Input placeholder="Phone" name="phone" defaultValue={singleRow?.phone} />
                 </Form.Item>
               </Col>
             </Row>
             <Form.Item name="eligibility">
               <Input
+              name="eligibility"
                 placeholder="Eligibility"
                 defaultValue={singleRow?.eligibility}
               />
             </Form.Item>
             <Form.Item name="bio">
               <Input.TextArea
+              name="bio"
                 rows={4}
                 placeholder="Bio"
                 defaultValue={singleRow?.bio}
