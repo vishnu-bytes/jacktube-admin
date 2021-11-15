d7f261c13bb2866602062e39e0c446a029487bda
Vishnu <vishnuur@techbyheart.in>
2021-11-15 15:23:23 +0530

    delete in expert

--- src/presentation/Experts/overview/ViewExperts.js
+++ src/presentation/Experts/overview/ViewExperts.js
@@ -8,27 +8,25 @@ import { useStudentStore } from "../store";
 import { logError } from "../../common/Utils";
 import { onEdit } from "../../../infrastructure/faculty";
 import ViewCards from "../../common/ViewCards";
-import { RowContainer } from "../../common/ViewCards/style.js"
-
+import { RowContainer } from "../../common/ViewCards/style.js";
 
 const { Option } = Select;
 
-
-
 const EditCategory = () => {
   const [form] = Form.useForm();
-  const [{ viewVisible, singleRow ,serviceList}, { onDelete, setViewVisible }] =
-    useStudentStore();
+  const [
+    { viewVisible, singleRow, serviceList },
+    { onDelete, setViewVisible },
+  ] = useStudentStore();
   // console.log(singleRow.qualifications, "single course");
 
-  const getServiceText=(id)=>{
-  for(let i=0;i<serviceList.length;i++){
-    if(id===serviceList[i].id){
-      return serviceList[i].title;
+  const getServiceText = (id) => {
+    for (let i = 0; i < serviceList.length; i++) {
+      if (id === serviceList[i].id) {
+        return serviceList[i].title;
+      }
     }
-  }
-
-  }
+  };
   return (
     <Modal
       type="primary"
@@ -44,17 +42,6 @@ const EditCategory = () => {
             onClick={() => setViewVisible(false)}
           >
             Cancel
-          </Button>
-          <Button
-            form="editProject"
-            size="default"
-            htmlType="submit"
-            type="primary"
-            key="submit"
-            danger
-            onClick={() => onDelete(singleRow?.phone,singleRow?.services)}
-          >
-            Delete
           </Button>
         </div>,
       ]}
@@ -72,20 +59,25 @@ const EditCategory = () => {
         <ViewCards label="Experience" value={singleRow?.experience} />
         <RowContainer>
           <span className="label">Qualifications </span>
-          <span className="value">{singleRow?.qualifications?.map((item, index) => (
-            <span key={index}>{index ? ', ' : ""}
-              {item}
-            </span>
-          ))}</span>
+          <span className="value">
+            {singleRow?.qualifications?.map((item, index) => (
+              <span key={index}>
+                {index ? ", " : ""}
+                {item}
+              </span>
+            ))}
+          </span>
         </RowContainer>
         <RowContainer>
           <span className="label">Services </span>
-          <span className="value">{singleRow?.services?.map((item, index) => (
-            <span key={index}>{index ? ', ' : ""}
-              {getServiceText(item)}
-            </span>
-          ))}</span>
-
+          <span className="value">
+            {singleRow?.services?.map((item, index) => (
+              <span key={index}>
+                {index ? ", " : ""}
+                {getServiceText(item)}
+              </span>
+            ))}
+          </span>
         </RowContainer>
         <ViewCards label="Bio" value={singleRow?.bio} />
         <ViewCards
